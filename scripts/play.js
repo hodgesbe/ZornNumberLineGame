16fe6733013ff4bb46193c0b3c170d9963cc838a
Ben <bnowak1@unca.edu>
2015-09-23 16:07:32 -0400

    Got the tester files working

--- scripts/play.js
+++ scripts/play.js
@@ -8,56 +8,63 @@ try{
 } catch(e) {}
 
 function init(){
-
-}
 //  Resize factor
-var resReduce = 4;
-
+    var canvasSizeReduce = 1;
 //  Get window size from browser
-var canvasWidth = window.innerWidth / resReduce;
-var canvasHeight = window.innerHeight / resReduce;
-
-
-//  Create a renderer object
-var renderer = new THREE.WebGLRenderer({antialias:true});
-//  Set the size of the renderer to the window size from above
-renderer.setSize(canvasWidth , canvasHeight);
-renderer.setClearColor(0x000000,1);
-//  Attache the renderer to the DOM
-document.body.appendChild(renderer.domElement);
+    var canvasWidth = window.innerWidth / canvasSizeReduce;
+    var canvasHeight = window.innerHeight / canvasSizeReduce;
 
 //  Create a new scene
-var scene = new THREE.Scene();
+    var scene = new THREE.Scene();
 
 //  Create a new camera using PerspectiveCamera(FOV, ratio, closestDrawableObject, farthestDrawableObject);
 //  Field Of View, Ratio (usually canvasHeight/canvasWidth)
-var camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 0.1, 1000);
+    var camera = new THREE.PerspectiveCamera(45, canvasWidth / canvasHeight, 0.1, 1000);
+
+//  Create a renderer object
+    var renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setClearColor(0xEEEEEE);
+    renderer.setSize(canvasWidth , canvasHeight);
+
+//  New Geometry Object
+    var planeGeometry = new THREE.PlaneGeometry(60,20,1,1);
+    var planeMaterial = new THREE.MeshBasicMaterial({color: 0xcccccc});
+    var plane = new THREE.Mesh(planeGeometry,planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.x = 15;
+    plane.position.y = 0;
+    plane.position.z = 0;
+
+    scene.add(plane);
+
+//  New Geometry Object
+    var cubeGeometry = new THREE.BoxGeometry(4,4,4);
+    var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+    cubeMaterial.wireframe = true;
+    var cube = new THREE.Mesh(cubeGeometry,cubeMaterial);
+
+    cube.position.x = -4;
+    cube.position.y = 3;
+    cube.position.z = 0;
+
+    scene.add(cube);
 
 //  Position the camera in relation to the origin
-camera.position.y = 160;
-camera.position.z = 400;    //  Z coordinate is the positive direction towards the viewer (ie: higher = closer)
+    camera.position.x = -30;
+    camera.position.y = 40;
+    camera.position.z = 30;  //  Z coordinate is the positive direction towards the viewer (ie: higher = closer)
+    camera.lookAt(scene.position);
+
+
+//  Attache the renderer to the DOM
+    document.getElementById("WebGL-output").appendChild(renderer.domElement);
+    renderer.render(scene, camera);
+
 
 //  Add camera to scene
-scene.add(camera);
+    scene.add(camera);
 
-//  Create cube and material objects
-var cubeGeometry = new THREE.CubeGeometry(10,10,10);
-var cubeMaterial = new THREE.MeshLambertMaterial( {color: 0x1ec876} );
-//  Use the cube and material objects to create a new mesh
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-//  Position the cube to the origin
-cube.rotation.y = Math.PI * 15 / 180;
-
-//  Add the cube to the scene
-scene.add(cube);
-
-//  Render the scene
-renderer.render(scene, camera);
-
-
-
-
+}
 //  DEBUGGING CODE
 function webgl_detect()
 {
